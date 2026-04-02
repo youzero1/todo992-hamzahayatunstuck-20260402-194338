f01bfc37f448f27d8c178ddebe0c99d8e770087e
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-04-02 19:45:24 +0500

    Initial commit from Flare Codegen (API)

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Todo } from '@/app/page';
+import TodoItem from './TodoItem';
+
+interface TodoListProps {
+  todos: Todo[];
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
+  onEdit: (id: string, newText: string) => void;
+}
+
+export default function TodoList({ todos, onToggle, onDelete, onEdit }: TodoListProps) {
+  return (
+    <div className="space-y-2">
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onToggle={onToggle}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
+      ))}
+    </div>
+  );
+}
